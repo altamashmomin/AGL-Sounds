8727b97d773fe9346afd8e1bdf13bef16c60205c
Star Lord <altamashymomin@gmail.com>
2018-05-31 01:29:18 -0400

    added slideshow with boxshadow effects

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,28 +1,3 @@
-var images = ["/Users/altamashmomin/Desktop/AGL-Sounds/assets/images/images copy/workspace.jpg", 
-                "/Users/altamashmomin/Desktop/AGL-Sounds/assets/images/images copy/workspace2.jpg",
-                "/Users/altamashmomin/Desktop/AGL-Sounds/assets/images/images copy/workspace3.jpg",
-                "/Users/altamashmomin/Desktop/AGL-Sounds/assets/images/images copy/workspace4.jpg"]
-
-var showimages;
-var count = 0;
-
-function displayImage() {
-    $("#workspace").html("<img src=" + images[count] + " width='auto'>");
-}
-
-function nextImage() {
-    count +=1;
-
-setTimeout(displayImage, 5000);
- if (count === images.length) {
-     count = 0;
-}
-
-function startSlideshow() {
-    showImages = setInterval(nextImage, 3000);
-  }
-}
-
-displayImage();
-console.log(displayImage());
-
+//$(document).ready(function() {
+    //jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 1000  // 2 seconds
+  //});
